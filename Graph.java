b8ad4dbf6042703674faae8d9be0db704dfd7176
Martin Evertsson <60136392+Drantj@users.noreply.github.com>
2020-05-20 13:09:55 +0200

    Add files via upload
    comments for main file and updated project diary

--- Graph.java
+++ Graph.java
@@ -9,12 +9,20 @@ import java.util.Scanner;
 import java.util.Stack;
 
 class Graph {
+	
+	// initiate hashmaps for storage
 	HashMap<Integer, LinkedList<Integer>> map = new HashMap<>();
-	HashMap<Integer, LinkedList<Integer>> degreeDist = new HashMap<>();
+	HashMap<Integer, LinkedList<Integer>> degreeDist = new HashMap<>();	
 	HashMap<Integer, Integer> cComponent = new HashMap<>();
 	
+	
+	/*
+	 * method for adding an edge from two vertices.
+	 * Takes two integers representing the vertex location in the map.
+	 */
 	public void addEdge(int u, int v) {
-		if (!map.containsKey(u)) {
+		// check if u is already in the map, if so add the v vertex to the corresponding LL.
+		if (!map.containsKey(u)) {				
 			LinkedList<Integer> l = new LinkedList<>();
 			l.add(v);
 			map.put(u, l);
@@ -26,6 +34,7 @@ class Graph {
 				map.put(u, l);
 			}
 		}
+		// same procedure as with u, since I am assuming we are creating undirected graphs.
 		if (!map.containsKey(v)) {
 			LinkedList<Integer> k = new LinkedList<>();
 			k.add(u);
@@ -40,21 +49,30 @@ class Graph {
 		}
 	}
 	
+	/*
+	 * method for testing how accurate the adjacencylist map is. Clearly meant for testing on
+	 * smaller graphs.
+	 */
 	void displayGraph() {
 		for (Map.Entry m: map.entrySet()) {
 			System.out.println(m.getKey() + "-->" + m.getValue());
 		}
 	}
 	
+	/*
+	 * method to get the node degree of the given vertex.
+	 * returns the size of the respective LL.
+	 */
 	private int nodeDegree(int u) {
 		if (map.containsKey(u)) {
-			int degree = map.get(u).size();
+			int degree = map.get(u).size();		// finds the size of the LL at the map location u.
 			return degree;
 		}
 		else {
 			return -1;
 		}
 	}
+	
 	
 	void degreeDistribution() {
 		for (int n = 0; n < map.size(); n++) {
@@ -81,25 +99,29 @@ class Graph {
 		
 	}
 	
-	public int size() {
-		return map.size();
-	}
-	
+	/*
+	 * method takes an integer and a boolean list and finds all elements that are connected to it
+	 * using the DFS algorithm. Returns a LL consisting of the components.
+	 */
 	public LinkedList<Integer> DFS(int v, boolean[] visited){
 		LinkedList<Integer> component = new LinkedList<Integer>(); 
+		// create a stack for DFS
 		Stack<Integer> stack = new Stack<>();
-		
+		// push the source vertex into the stack
 		stack.push(v);
 		
 		while(!stack.empty()) {
 			
+			// pop a vertex and print it
 			v = stack.peek();
 			stack.pop();
 			
+			// if given vertex has NOT been visited before, add component and mark it as visited.
 			if(!visited[v]) {
 				component.add(v);
 				visited[v] = true;
 			}
+			// get all adjacent vertices of the popped vertex, if NOT visited, push to stack.
 			for (int x : map.get(v)) {
 				if(!visited[x]) {
 					stack.push(x);
@@ -108,14 +130,22 @@ class Graph {
 		}
 		return component;
 	}
-	
+
+	/*
+	 * method for finding the number of connected components distribution in the graph.
+	 * Uses the DFS method to update the cComponent map with the number of connected
+	 * components of that particular size. Returns void.
+	 */
 	void connectedComponents() {
+		// Use the same boolean for each time we use DFS to know where we have been
 		boolean[] visited = new boolean[map.size()];
 		
+		// iterate over all vertices to fill the visited array.
 		for ( int v = 0; v < map.size(); v++) {
 			if(!visited[v]) {
 				LinkedList<Integer> component = DFS(v, visited);
 				int cSize = component.size();
+				// get size and add to relevant map location.
 				if (!cComponent.containsKey(cSize)) {
 					cComponent.put(cSize, 1);
 				}
@@ -126,13 +156,14 @@ class Graph {
 			}
 		}
 	}
-	void display_components() {
-		System.out.println(cComponent);
-	}
-		
 	
+		
+	/*
+	 * method to find the number of connected components in graph G.
+	 */
 	public int ccNumber() {
 		int tot = 0;
+		// iterate cComponents and add up all values in the current position. 
 		for (int value : cComponent.values()) {
 			tot = tot + value;
 		}
@@ -151,23 +182,29 @@ class Graph {
 		System.out.println();
 	}
 	
+	/*
+	 * main program reads from System.in to produce a graph and calculate various 
+	 * properties of the graph. Meant to be run in an unix tool.
+	 */
 	public static void main(String[] args) throws NoSuchElementException {
+		
 		long startTime = System.currentTimeMillis();
 		Graph g = new Graph();
 		
 		Scanner sc = new Scanner(System.in);
 		int i = 0;
 		HashMap<String, Integer> vertex = new HashMap<>();
-		//ArrayList<String> vertex = new ArrayList<String>();
 		
-		try {			// build the graph bases on scanned entries from System.in
+		try {			// build the graph based on scanned entries from System.in
 			while (sc.hasNextLine()) {
+				// first two values are vertices in the graph
 				String u = sc.next();
 				String v = sc.next();
-			
+				
+				// if not found in the vertex map, add it put the value as i
 				if (!vertex.containsKey(u)) {
 					vertex.put(u, i);
-					i++;
+					i++;	// update i
 				}
 				if (!vertex.containsKey(v)) {
 					vertex.put(v, i);
@@ -176,17 +213,16 @@ class Graph {
 				
 				int uNum = vertex.get(u);
 				int vNum = vertex.get(v);
-			
+				
+				// add the edge using the integer value of i at the location of the string
 				g.addEdge(uNum,  vNum);
 			
 			}
 		}
 		catch(Exception e) {}
 		g.connectedComponents();
-		//g.displayGraph();
 		
 		System.out.println("------------------------------------");
-		System.out.println(g.size());
 		System.out.println("The node degree distribution of G is represented by:");
 		
 		g.degreeDistribution();
